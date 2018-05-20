362342b0a14df78b84a0e71e4e847bda86fb57a0
yangbowen <1271316718@qq.com>
2018-05-20 17:03:58 +0800

    first commit

---
+++ app/src/main/java/net/baiken/githubdemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package net.baiken.githubdemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,SecondActivity::class.java))
+    }
+}
---
+++ app/src/main/java/net/baiken/githubdemo/SecondActivity.java
@@ -0,0 +1,21 @@
+package net.baiken.githubdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <pre>
+ *     author : yangbowen
+ *     time   : 2018/05/20
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
